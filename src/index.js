06eff0ae23cca77c3f26257e8aabcdc97b611327
ruiz.terce <ruiz.terce@gmail.com>
2024-04-20 20:38:03 +0200

    Move homepage elements from index.html to load.js
    Remove elements inside #content in index.html.
    Add new module in load.js to generate those
    elements and export load method to index.js.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import load from "./load.js";
+
+load();
---
+++ src/load.js
@@ -0,0 +1,30 @@
+const load = () => {
+    const contentDiv = document.querySelector('#content')
+
+    const h1Div = document.createElement('h1');
+    h1Div.textContent = 'Yuzu Blossom';
+
+    const h2Div = document.createElement('h2');
+    h2Div.textContent = 'Japanese Fusion Cuisine';
+
+    const p1Div = document.createElement('p');
+    p1Div.textContent = `Welcome to Yuzu Blossom, where culinary innovation meets traditional Japanese craftsmanship. Nestled in the
+    heart of Granada, Spain, our restaurant invites you on a gastronomic adventure like no other. From the
+    moment
+    you step inside, you're transported to a realm where every dish tells a story, where each bite is a symphony
+    of flavors.`;
+
+    const p2Div = document.createElement('p');
+    p2Div.textContent = `Our menu is a testament to our dedication to excellence, blending the finest ingredients with creative flair
+    to create a dining experience that's both familiar and exhilaratingly new. Whether you're craving the
+    delicate balance of sushi rolls infused with unexpected ingredients, or the robust umami of our signature
+    ramen bowls, every dish is a masterpiece crafted with care by our talented chefs.`
+
+
+    contentDiv.appendChild(h1Div);
+    contentDiv.appendChild(h2Div);
+    contentDiv.appendChild(p1Div);
+    contentDiv.appendChild(p2Div);
+};
+
+export { load as default }
