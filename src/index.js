66a3098827c4043948569745a0cb11f0e18cb573
ruiz.terce <ruiz.terce@gmail.com>
2024-04-21 14:27:21 +0200

    Add id to nav buttons and add event listeners

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import loadHome from "./loadHome.js";
 loadHome();
 
 
+const homeBtn = document.querySelector('#homeBtn')
+homeBtn.addEventListener('click', () => { loadHome(); });
+
+const menuBtn = document.querySelector('#menuBtn');
+menuBtn.addEventListener('click', () => { });
+
+const aboutBtn = document.querySelector('#aboutBtn');
+menuBtn.addEventListener('click', () => { });
