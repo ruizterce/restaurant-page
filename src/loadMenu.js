211c95c3e4300ebf22a0c1419b4e06abbe250ada
ruiz.terce <ruiz.terce@gmail.com>
2024-04-21 17:45:06 +0200

    Add some elements and specific style to Menu page
    Add some elements to menu page through loadMenu():
    h1 and a first static .dish element with image
    and text areas
    
    Add initial styling for menu page in style.css

--- src/loadMenu.js
+++ src/loadMenu.js
@@ -1,3 +1,6 @@
+
+import fish from './img/fish.png'
+
 const loadMenu = () => {
     const contentDiv = document.querySelector('#content')
 
@@ -7,7 +10,43 @@ const loadMenu = () => {
     h1Div.className = 'menu';
     h1Div.textContent = 'Menu';
 
+    const dishArea = document.createElement('div');
+    dishArea.id = 'dishArea';
+    dishArea.className = 'menu';
+
+    const dish = document.createElement('div');
+    dish.className = 'menu dish';
+
+    const iconWrapper = document.createElement('div');
+    iconWrapper.className = 'menu iconWrapper';
+
+    const img = document.createElement('img');
+    img.src = fish;
+
+    iconWrapper.appendChild(img);
+
+    const textWrapper = document.createElement('div');
+    textWrapper.className = 'menu textWrapper';
+
+    const dishName = document.createElement('h4');
+    dishName.className = 'menu';
+    dishName.textContent = 'Sushi';
+
+    const dishDesc = document.createElement('p');
+    dishDesc.className = 'menu';
+    dishDesc.textContent = 'Delicious maki roll';
+
+    textWrapper.appendChild(dishName);
+    textWrapper.appendChild(dishDesc);
+
+    dish.appendChild(iconWrapper);
+    dish.appendChild(textWrapper);
+
+    dishArea.appendChild(dish);
+
+
     contentDiv.appendChild(h1Div);
+    contentDiv.appendChild(dishArea);
 
 };
 
